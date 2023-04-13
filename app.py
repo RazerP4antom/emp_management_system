b51b611eed91f6e2d7fed4c1955f4ec3584af4b0
RazerP4antom <mirjisarthak3@gmail.com>
2023-04-13 12:51:49 +0530

    initial setup home page

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, url_for
+from flask.templating import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello</h1>'
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/add_new_emp')
+def add_new_emp():
+    return render_template('addNewEmp.html')
+
+@app.route('/update_emp')
+def update_emp():
+    return render_template('updateEmp.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
