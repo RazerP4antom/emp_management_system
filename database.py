3afc081da4acea5fb8a86efaa1338a8622306a79
RazerP4antom <mirjisarthak3@gmail.com>
2023-04-14 13:32:55 +0530

    database created

---
+++ database.py
@@ -0,0 +1,12 @@
+from flask import g
+import sqlite3
+
+def connection():
+    conn = sqlite3.connect('C:/Users/mirji/personal projects/emp_management_system/crudapp.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_database():
+    if not hasattr(g, 'crudapp_db'):
+        g.crudapp_db = connection()
+    return g.crudapp.db
